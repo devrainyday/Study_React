b38e6954988124be3e46da93f2808c92623dafb6
kyw017763 <kyw017763@gmail.com>
2019-07-30 02:47:36 +0900

    Setting, JSX

---
+++ hello-react/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component, Fragment } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <Fragment>
+        <div className="App">
+          리액트
+        </div>
+        
+        <div>
+          <div>
+          {/* asdf */}
+          <h1
+            // asdf
+          >리액트</h1>
+          </div>
+        </div>
+      </Fragment>
+    );
+}
+
+export default App;
